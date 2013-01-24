9fc4fea4d5a09ea3f235d2a7e5d644ff6804dc64
ishu3101 <ishu3101@hotmail.com>
2013-01-24 15:19:02 +1300

    Added Simple Live Validation and CSS to Order Forms.

--- test/functional/orders_controller_test.rb
+++ test/functional/orders_controller_test.rb
@@ -12,13 +12,20 @@ class OrdersControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    item = LineItem.new
+    item.build_cart
+    item.product = products(:ruby)
+    item.save!
+    session[:cart_id] = item.cart.id
+
     get :new
     assert_response :success
+
   end
 
   test "should create order" do
     assert_difference('Order.count') do
-      post :create, order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type }
+      post :create, order: {address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type}
     end
 
     assert_redirected_to order_path(assigns(:order))
@@ -35,7 +42,7 @@ class OrdersControllerTest < ActionController::TestCase
   end
 
   test "should update order" do
-    put :update, id: @order, order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type }
+    put :update, id: @order, order: {address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type}
     assert_redirected_to order_path(assigns(:order))
   end
 
@@ -46,4 +53,11 @@ class OrdersControllerTest < ActionController::TestCase
 
     assert_redirected_to orders_path
   end
+
+  test "requires item in cart" do
+    get :new
+    assert_redirected_to store_path
+    assert_equal flash[:notice], 'Your cart is empty'
+  end
+
 end
