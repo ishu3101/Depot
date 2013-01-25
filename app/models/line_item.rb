b235ffc148328463b69bbf8b53e8bf85327f566a
ishu3101 <ishu3101@hotmail.com>
2013-01-25 13:53:39 +1300

    Capturing Order Details.

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,6 +2,7 @@ class LineItem < ActiveRecord::Base
   attr_accessible :cart_id, :product_id
   #attr_accessible :cart_id, :product_id, :product, :cart
   #attr_accessible :cart_id, :product_id, :quantity, :order_id, :product, :cart
+  belongs_to :order
   belongs_to :product
   belongs_to :cart
 
