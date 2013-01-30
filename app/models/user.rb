90b88afdc9db06da785360be01e6dfa742763c62
ishu3101 <ishu3101@hotmail.com>
2013-01-31 12:00:13 +1300

    Added Sidebar and fix having no administrative users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,16 @@ class User < ActiveRecord::Base
   attr_accessible :name, :password, :password_confirmation
   validates :name, presence: true, uniqueness: true
   has_secure_password
+
+  # If after we’ve deleted the user, there are no users left in the
+  #database, we will roll the transaction back (raising an exception does that), restoring the user we just deleted.
+  after_destroy :ensure_an_admin_remains
+
+  private
+  def ensure_an_admin_remains
+    if User.count.zero?
+      raise "Can't delete last user"
+    end
+  end
+
 end
